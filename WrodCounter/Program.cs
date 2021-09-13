09f039b4ef472a35b31086ddafb31d57703b7a24
hamidramezanali <hamid.ra2018@gmail.com>
2021-09-13 15:30:32 +0200

    Little bit refactoring

--- WrodCounter/Program.cs
+++ WrodCounter/Program.cs
@@ -9,62 +9,56 @@ namespace WrodCounter
 {
     class Program
     {
-        static List<string> output = new List<string>();
+        
         static void Main(string[] args)
         {
-            string pat = @"(def)\s(\w+)\s(-?(1000|[0-9][0-9][0-9]|[0-9][0-9]|[0-9])$)";
-            Regex regex = new Regex(pat, RegexOptions.IgnoreCase);       
-            List<string> fileLines=FileReader.Read();
-            
+            string pathIn = args[0]; //Full path to file >> C:\XXX\Sample.In
+            string pathOut = args[1]; //Full path to file >> C:\XXX\Sample.Out
 
-            foreach (var line in fileLines)
+            List<string> output = new List<string>();
+            List<string> input=FileReader.Read(pathIn);            
+
+            foreach (var line in input)
             {
-                GetCategory(regex, line);
-
+                var func = line.Split(' ').FirstOrDefault();
+                switch (func)
+                {                
+                    case "def" : MyDictionary.DefHandler(line); break;                
+                    case "calc": output.Add(MyCalculator.CalcHandler(line)); break;
+                    case "clear": MyDictionary.ClearTable(); break;
+                    default:break;
+                } 
             }
-            FileReader.Write(output);
-        }
 
-        private static void GetCategory(Regex regex, string line)
+            FileReader.Write(pathOut,output);
+        } 
+
+ 
+    }
+    #region MyCalculator
+    public static class MyCalculator
+    {
+        public static string CalcHandler(string calcLine)
         {
-            if (line.StartsWith("def"))
-            {
-                DefDetector(regex, line);
-            }
-            else if (line.StartsWith("calc"))
-            {
-                calcDetector(line);
-            }
-            else if (line.StartsWith("clear"))
-            {
-                MyDictionary.ClearTable();
-            }
-        }
-
-
-        private static void calcDetector(string calcLine)
-        {
-
             List<string> calcLineItems = calcLine.Split(' ').ToList();
             int? Total = CalculateTotal(calcLineItems);
-            PrintTotal(calcLine, Total);
+            return BuildOutputString(calcLine, Total);
         }
 
-        private static void PrintTotal(string calcLine, int? Total)
+        private static string BuildOutputString(string calcLine, int? Total)
         {
             StringBuilder sb = new StringBuilder();
-            sb.Append(@$"{calcLine.Remove(0, 4)}");
+            sb.Append(@$"{calcLine.Remove(0, 5)}");// Remove 'calc '
             if (Total != null)
                 if (MyDictionary.ContainsValue((int)Total))
-                { sb.AppendLine(@$" {MyDictionary.GetKey((int)Total)}"); output.Add(sb.ToString()); return; }
-            sb.AppendLine(@$" Unknown ");
-            output.Add(sb.ToString());
+                { sb.Append(@$"{MyDictionary.GetKey((int)Total)}"); return sb.ToString(); }
+            sb.Append(@$" unknown");
+            return sb.ToString();
         }
 
         private static int? CalculateTotal(List<string> values)
         {
-            Dictionary<string, int> sta = MyDictionary.sta;
-        int? numericEq;
+            int? numericEq;
             int i = 1;
             int? total = 0;
             int factor = 1;
@@ -99,56 +93,43 @@ namespace WrodCounter
                 }
                 return total;
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return null;
             }
         }
 
 
+    }
+    #endregion
+    #region MyDictionary
+    public static class MyDictionary
 
-
-
-
-    
-
-        private static void DefDetector(Regex re, string item)
+    {
+        public static Dictionary<string, int> VariableList = new Dictionary<string, int>();
+        public static void Add(string s, int i) { if (!VariableList.Any(_ => _.Key == s)) VariableList.Add(s, i); else VariableList[s] = i; }
+        public static bool ContainsValue(int i) => VariableList.Any(_ => _.Value == i);
+        public static int? GetValue(string s) => (VariableList.Any(_ => _.Key == s)) ? VariableList.Single(_ => _.Key == s).Value : null;
+        public static string GetKey(int i) => VariableList.Single(_ => _.Value == i).Key;
+        public static void ClearTable() => VariableList = new Dictionary<string, int>();
+        public static void DefHandler(string item)
         {
-            Match m = re.Match(item);
+            string pattern = @"(def)\s(\w+)\s(-?(1000|[0-9][0-9][0-9]|[0-9][0-9]|[0-9])$)";
+            Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
+            Match m = regex.Match(item);
+            string alphabeticalValue = m.Groups[2].Value;
+            int numericalValue = int.Parse(m.Groups[3].Value);
 
-            if (!MyDictionary.ContainsKey(m.Groups[2].Value))
-                MyDictionary.Add(m.Groups[2].Value, int.Parse(m.Groups[3].Value));
-            else
-            {
-                MyDictionary.UpdateValue(m.Groups[2].Value , int.Parse(m.Groups[3].Value));
+            VariableList.Add(alphabeticalValue, numericalValue);
 
-            }
         }
     }
-
-    public static class MyDictionary
-    {
-        public static Dictionary<string, int> sta = new Dictionary<string, int>();
-        public static void Add(string s, int i) => sta.Add(s, i);
-        public static bool ContainsKey(string s) => sta.Any(_ => _.Key == s);
-        public static bool ContainsValue(int i) => sta.Any(_ => _.Value == i);
-        public static int? GetValue(string s) => (sta.Any(_ => _.Key == s)) ? sta.Single(_ => _.Key == s).Value : null;
-        public static string GetKey(int i) => sta.Single(_ => _.Value == i).Key;
-        public static void UpdateValue(string s,int i) => sta[s]=i;
-        public static void ClearTable() => sta = new Dictionary<string, int>();
-    }
+    #endregion
+    #region FileReader
     public static  class FileReader
     {
-        static string Path = @"C:\Users\hamid\Downloads\sample.in";
-        static string PathOut = @"C:\Users\hamid\Downloads\sample3.out";
-        public static List<string> Read()
-        {
-            List<string> file = File.ReadAllLines(Path).ToList();
-            return file;
+        public static List<string> Read(string PathIn) => File.ReadAllLines(PathIn).ToList(); 
+        public static void Write(string PathOut,List<string> output) => File.WriteAllLines(PathOut, output);      
     }
-        public static void Write(List<string> output)
-        {
-            File.WriteAllLines(PathOut, output);
-        }
-    }
+    #endregion
 }
